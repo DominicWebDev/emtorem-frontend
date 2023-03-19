814f40a8e9992447894d9be84a69eca57df117e2
Dominic Matte <mattedominic@gmx.de>
2023-03-19 18:16:33 +0100

    fix(nextconfig): change scripts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
